f2642fdd90f018c208099f9b3e2e0cc36e0555c7
ZhongJack2020 <2214049261@qq.com>
2022-01-14 23:01:35 +0800

    recreate DataAnalyzer class

--- helpers/DataAnalyzer.py
+++ helpers/DataAnalyzer.py
@@ -3,7 +3,16 @@
 from helpers.GlobalValue import GlobalValue
 
 class DataAnalyzer:
-    data = {}
+    viewer_data = {}
 
     def __init__(self):
         pass
+
+    def setViewerData(self,tags,levels):
+        # set level
+        for key in levels:
+            self.viewer_data[key] = None
+
+
+    def getViewerData(self):
+        return self.viewer_data
